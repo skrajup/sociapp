1a421fd4660a6092426431711eb1303d7f3b7c67
NTSTET <skrajprof1947@gmail.com>
2022-06-05 22:22:00 +0530

    database setup & basic signin, signup implemnt

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.static("public"));
@@ -9,8 +10,61 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.get("/",(req, res)=>{
+mongoose.connect("mongodb://localhost:27017/sociappDB", {useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String,
+});
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res)=>{
     res.render("homepage");
+});
+
+app.get("/create-post", (req, res)=>{
+    res.render("create-post");
+})
+
+app.get("/profile", (req, res)=>{
+    res.render("profile");
+})
+
+
+app.post("/signup", (req, res)=>{
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    newUser.save((err) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.render("home-dashboard");
+        }
+    });
+});
+
+app.post("/signin", (req, res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+    
+    User.findOne({username: username}, (err, foundUser)=>{
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+                // console.log(foundUser);
+                if(foundUser.password === password){
+                    res.render("home-dashboard");
+                }
+            }
+        }
+    });
 });
 
 app.listen(3000, ()=>{
